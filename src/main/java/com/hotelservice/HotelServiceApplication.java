fc14af15373753ce103d0934b813731f436d25b4
setor <marik123456789110011@gmail.com>
2026-09-02 01:48:13 +0300

    feet: hotel entity

--- src/main/java/com/hotelservice/HotelServiceApplication.java
+++ src/main/java/com/hotelservice/HotelServiceApplication.java
@@ -1,4 +1,11 @@
 package com.hotelservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HotelServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelServiceApplication.class, args);
+    }
 }
---
+++ src/main/java/com/hotelservice/entity/Hotel.java
@@ -0,0 +1,22 @@
+package com.hotelservice.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "hotels")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String brand;
+
+}
